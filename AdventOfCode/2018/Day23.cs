c9b46ab8f5f4ada1498dff4b55582d8a53eeeca3
Mike Oliphant <oliphant@nostatic.org>
2022-04-30 08:42:29 -0700

    Day23

--- AdventOfCode/2018/Day23.cs
+++ AdventOfCode/2018/Day23.cs
@@ -1,11 +1,14 @@
-﻿namespace AdventOfCode._2018
+﻿using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+
+namespace AdventOfCode._2018
 {
     internal class Day23
     {
         class Nanobot
         {
-            public Vector3 Position { get; set; }
-            public float Range { get; set; }
+            public LongVec3 Position { get; set; }
+            public long Range { get; set; }
 
             public override string ToString()
             {
@@ -19,9 +22,9 @@
         {
             foreach (string line in File.ReadLines(@"C:\Code\AdventOfCode\Input\2018\Day23.txt"))
             {
-                Vector3 pos = new Vector3(Regex.Match(line, "<(.*)>").Groups[1].Value.ToFloats(',').ToArray());
+                LongVec3 pos = new LongVec3(Regex.Match(line, "<(.*)>").Groups[1].Value.ToLongs(',').ToArray());
 
-                float range = float.Parse(Regex.Match(line, "r=(.*)").Groups[1].Value);
+                long range = long.Parse(Regex.Match(line, "r=(.*)").Groups[1].Value);
 
                 bots.Add(new Nanobot {  Position = pos, Range = range });
             }
@@ -38,38 +41,97 @@
             return bots.Where(b => b.Position.ManhattanDistance(strongest.Position) <= strongest.Range).Count();
         }
 
+
+        struct OctBox
+        {
+            public LongVec3 Min { get; set; }
+            public long Size { get; set;  }
+            public int NumBots { get; set; }
+
+            public bool InRange(LongVec3 pos, long range)
+            {
+                long dist = 0;
+
+                if (pos.X < Min.X)
+                    dist += (Min.X - pos.X);
+                else if (pos.X >= (Min.X + Size))
+                    dist += (pos.X - (Min.X + Size - 1));
+
+                if (dist > range)
+                    return false;
+
+                if (pos.Y < Min.Y)
+                    dist += (Min.Y - pos.Y);
+                else if (pos.Y >= (Min.Y + Size))
+                    dist += (pos.Y - (Min.Y + Size - 1));
+
+                if (dist > range)
+                    return false;
+
+                if (pos.Z < Min.Z)
+                    dist += (Min.Z - pos.Z);
+                else if (pos.Z >= (Min.Z + Size))
+                    dist += (pos.Z - (Min.Z + Size - 1));
+
+                return (dist <= range);
+            }
+
+            public override string ToString()
+            {
+                return NumBots + " " + Min + " " + Size; 
+            }
+        }
+
+        List<OctBox> boundsToSearch = new List<OctBox>();
+
+        void DivideBounds(OctBox box)
+        {
+            long newSize = box.Size / 2;
+
+            for (int x = 0; x < 2; x++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    for (int z = 0; z < 2; z++)
+                    {
+                        OctBox newBox = new OctBox { Min = box.Min + new LongVec3(x * newSize, y * newSize, z * newSize), Size = newSize };
+
+                        newBox.NumBots = bots.Where(b => newBox.InRange(b.Position, b.Range)).Count();
+
+                        if (newBox.NumBots > 0)
+                            boundsToSearch.Add(newBox);
+                    }
+                }
+            }
+        }
+
+        // Needed some help from here: https://raw.githack.com/ypsu/experiments/master/aoc2018day23/vis.html
+
         public long Compute2()
         {
             ReadInput();
 
-            //Dictionary<Vector3, int> candidatePoints = new Dictionary<Vector3, int>();
+            long size = 1 << 30;
 
-            //for (int pos1 = 0; pos1 < bots.Count; pos1++)
-            //{
-            //    for (int pos2 = pos1 + 1; pos2 < bots.Count; pos2++)
-            //    {
-            //        if (bots[pos1].Position.ManhattanDistance(bots[pos2].Position) == (bots[pos1].Range + bots[pos2].Range))
-            //        {
-            //            Vector3 pos = (bots[pos1].Position + bots[pos2].Position) / 2;
+            DivideBounds(new OctBox { Min = new LongVec3(-size, -size, -size), Size = size * 2 });
 
-            //            candidatePoints[pos] = bots.Where(b => b.Position.ManhattanDistance(pos) <= b.Range).Count();
-            //        }
-            //    }
-            //}
-
-            //Vector3 bestPos = candidatePoints.OrderByDescending(p => p.Value).First().Key;
-
-
-            Dictionary<Nanobot, int> inRangeOf = new Dictionary<Nanobot, int>();
-
-            foreach (Nanobot bot in bots)
+            do
             {
-                inRangeOf[bot] = bots.Where(b => b.Position.ManhattanDistance(bot.Position) <= bot.Range).Count();
+                OctBox next = boundsToSearch.OrderByDescending(o => o.NumBots).ThenBy(o => o.Min.ManhattanDistance(LongVec3.Zero)).ThenBy(o => o.Size).First();
+
+                if (next.Size == 1)
+                {
+                    return (long)(Math.Abs(next.Min.X) + Math.Abs(next.Min.Y) + Math.Abs(next.Min.Z));
+                }
+
+                boundsToSearch.Remove(next);
+
+                DivideBounds(next);
             }
+            while (true);
 
-            var sorted = inRangeOf.OrderBy(r => r.Value).ToArray();
-
-            return 0;
+            throw new InvalidOperationException();
         }
     }
 }
+
---
+++ AdventOfCode/LongVec3.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode
+{
+    class LongVec3
+    {
+        public long X { get; set; }
+        public long Y { get; set; }
+        public long Z { get; set; }
+
+        public LongVec3(long x, long y, long z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public LongVec3(long[] components)
+        {
+            this.X = components[0];
+            this.Y = components[1];
+            this.Z = components[2];
+        }
+
+        public static LongVec3 Zero
+        {
+            get { return new LongVec3(0, 0, 0); }
+        }
+
+        public override string ToString()
+        {
+            return X + ", " + Y + ", " + Z;
+        }
+
+        public long ManhattanDistance(LongVec3 other)
+        {
+            return Math.Abs(X - other.X) + Math.Abs(Y - other.Y) + Math.Abs(Z - other.Z);
+        }
+
+        public static LongVec3 operator +(LongVec3 a, LongVec3 b) => new LongVec3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+        public static LongVec3 operator -(LongVec3 a, LongVec3 b) => new LongVec3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+        public static LongVec3 operator *(LongVec3 a, LongVec3 b) => new LongVec3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+
+        public static LongVec3 operator /(LongVec3 a, LongVec3 b) => new LongVec3(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+    }
+}
