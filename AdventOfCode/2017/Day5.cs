2ae98ef6cd5d4950ab6cb8bbccad305daaa907d9
Mike Oliphant <oliphant@nostatic.org>
2022-05-02 10:47:35 -0700

    Day5

---
+++ AdventOfCode/2017/Day5.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode._2017
+{
+    internal class Day5
+    {
+
+        public long Compute()
+        {
+            int[] jumps = File.ReadLines(@"C:\Code\AdventOfCode\Input\2017\Day5.txt").ToInts().ToArray();
+
+            //int[] jumps = new int[] { 0, 3, 0, 1, -3 };
+
+            int instructionPos = 0;
+
+            int step = 0;
+
+            do
+            {
+                int lastPos = instructionPos;
+
+                instructionPos += jumps[instructionPos];
+
+                jumps[lastPos]++;
+
+                step++;
+            }
+            while (instructionPos < jumps.Length);
+
+            return step;
+        }
+
+        public long Compute2()
+        {
+            int[] jumps = File.ReadLines(@"C:\Code\AdventOfCode\Input\2017\Day5.txt").ToInts().ToArray();
+
+            int instructionPos = 0;
+
+            int step = 0;
+
+            do
+            {
+                int lastPos = instructionPos;
+
+                instructionPos += jumps[instructionPos];
+
+                if (jumps[lastPos] >= 3)
+                {
+                    jumps[lastPos]--;
+                }
+                else
+                {
+                    jumps[lastPos]++;
+                }
+
+                step++;
+            }
+            while (instructionPos < jumps.Length);
+
+            return step;
+        }
+    }
+}
