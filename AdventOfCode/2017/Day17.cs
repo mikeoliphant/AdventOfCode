84123c32665085304d467eda2b90c479e7b40e05
Mike Oliphant <oliphant@nostatic.org>
2022-05-04 12:07:39 -0700

    more effecient solution for Day 17 part 2

--- AdventOfCode/2017/Day17.cs
+++ AdventOfCode/2017/Day17.cs
@@ -24,7 +24,7 @@
             return pos.MoveCircular(1).Value;
         }
 
-        public long Compute2()
+        public long Compute2BruteForce()
         {
             int stepSize = 329;
 
@@ -44,6 +44,31 @@
             }
 
             return pos.MoveCircular(1).Value;
+        }
+
+        public long Compute2()
+        {
+            int stepSize = 329;
+
+            int pos = 0;
+            int listSize = 1;
+            int pos1Val = 0;
+
+            for (int turn = 0; turn < 50000000; turn++)
+            {
+                pos = (pos + stepSize) % listSize;
+
+                listSize++;
+
+                pos = (pos + 1) % listSize;
+
+                if (pos == 1)
+                {
+                    pos1Val = turn + 1;
+                }
+            }
+
+            return pos1Val;
         }
     }
 }
