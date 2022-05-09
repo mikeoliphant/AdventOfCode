ca1d025074d7edf82383898242861d00cc841650
Mike Oliphant <oliphant@nostatic.org>
2022-05-09 11:21:43 -0700

    Day 6

---
+++ AdventOfCode/2016/Day6.cs
@@ -0,0 +1,86 @@
+﻿namespace AdventOfCode._2016
+{
+    internal class Day6
+    {
+
+        public long Compute()
+        {
+            Dictionary<char, int>[] posHist = null;
+
+            foreach (string msg in File.ReadLines(@"C:\Code\AdventOfCode\Input\2016\Day6.txt"))
+            {
+                if (posHist == null)
+                {
+                    posHist = new Dictionary<char, int>[msg.Length];
+
+                    for (int pos = 0; pos < posHist.Length; pos++)
+                    {
+                        posHist[pos] = new Dictionary<char, int>();
+                    }
+                }
+
+                for (int pos = 0; pos < posHist.Length; pos++)
+                {
+                    if (!posHist[pos].ContainsKey(msg[pos]))
+                    {
+                        posHist[pos][msg[pos]] = 1;
+                    }
+                    else
+                    {
+                        posHist[pos][msg[pos]]++;
+                    }
+                }
+            }
+            
+
+            string message = "";
+
+            for (int pos = 0; pos < posHist.Length; pos++)
+            {
+                message += posHist[pos].OrderByDescending(p => p.Value).First().Key;
+            }
+
+            return 0;
+        }
+
+        public long Compute2()
+        {
+            Dictionary<char, int>[] posHist = null;
+
+            foreach (string msg in File.ReadLines(@"C:\Code\AdventOfCode\Input\2016\Day6.txt"))
+            {
+                if (posHist == null)
+                {
+                    posHist = new Dictionary<char, int>[msg.Length];
+
+                    for (int pos = 0; pos < posHist.Length; pos++)
+                    {
+                        posHist[pos] = new Dictionary<char, int>();
+                    }
+                }
+
+                for (int pos = 0; pos < posHist.Length; pos++)
+                {
+                    if (!posHist[pos].ContainsKey(msg[pos]))
+                    {
+                        posHist[pos][msg[pos]] = 1;
+                    }
+                    else
+                    {
+                        posHist[pos][msg[pos]]++;
+                    }
+                }
+            }
+
+
+            string message = "";
+
+            for (int pos = 0; pos < posHist.Length; pos++)
+            {
+                message += posHist[pos].OrderBy(p => p.Value).First().Key;
+            }
+
+            return 0;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -15,7 +15,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            long result = new AdventOfCode._2016.Day5().Compute2();
+            long result = new AdventOfCode._2016.Day6().Compute2();
         }
     }
 }
