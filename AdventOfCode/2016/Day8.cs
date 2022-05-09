af2de5efaf7365170779c640573cb96f5fdb27e3
Mike Oliphant <oliphant@nostatic.org>
2022-05-09 12:52:59 -0700

    Day 8

---
+++ AdventOfCode/2016/Day8.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode._2016
+{
+    internal class Day8
+    {
+        public long Compute()
+        {
+            //Grid<char> grid = new Grid<char>(7, 3);
+            Grid<char> grid = new Grid<char>(50, 6);
+
+            foreach (string cmd in File.ReadLines(@"C:\Code\AdventOfCode\Input\2016\Day8.txt"))
+            {
+                var match = Regex.Match(cmd, "rect (.*)x(.*)");
+
+                if (match.Success)
+                {
+                    foreach (var g in grid.GetRectangle(new Rectangle(0, 0, int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value))))
+                    {
+                        grid.SetValue(g.X, g.Y, '#');
+                    }
+                }
+                else
+                {
+                    match = Regex.Match(cmd, "rotate column x=(.*) by (.*)");
+
+                    if (match.Success)
+                    {
+                        int x = int.Parse(match.Groups[1].Value);
+                        int rot = int.Parse(match.Groups[2].Value);
+
+                        Grid<char> newGrid = new Grid<char>(grid);
+
+                        for (int y = 0; y < grid.Height; y++)
+                        {
+                            newGrid[x, (y + rot) % grid.Height] = grid[x, y];
+                        }
+
+                        grid = newGrid;
+                    }
+                    else
+                    {
+                        match = Regex.Match(cmd, "rotate row y=(.*) by (.*)");
+
+                        if (match.Success)
+                        {
+                            int y = int.Parse(match.Groups[1].Value);
+                            int rot = int.Parse(match.Groups[2].Value);
+
+                            Grid<char> newGrid = new Grid<char>(grid);
+
+                            for (int x = 0; x < grid.Width; x++)
+                            {
+                                newGrid[(x + rot) % grid.Width, y] = grid[x, y];
+                            }
+
+                            grid = newGrid;
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException();
+                        }
+                    }
+                }
+                
+            }
+
+            grid.PrintToConsole();
+
+            return grid.CountValue('#');
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -15,7 +15,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            long result = new AdventOfCode._2016.Day7().Compute2();
+            long result = new AdventOfCode._2016.Day8().Compute();
         }
     }
 }
