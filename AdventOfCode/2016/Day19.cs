e0e575d8d958f36e6788af1f7c250311b81c31f6
Mike Oliphant <oliphant@nostatic.org>
2022-05-12 11:07:57 -0700

    Day 19

---
+++ AdventOfCode/2016/Day19.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode._2016
+{
+    internal class Day19
+    {
+        LinkedList<int> elves = new LinkedList<int>();
+
+        public long Compute()
+        {
+            int numElves = 3012210;
+
+            for (int i = 0; i < numElves; i++)
+            {
+                elves.AddLast(i + 1);
+            }
+
+            var elf = elves.First;
+
+            do
+            {
+                elves.Remove(elf.MoveCircular(1));
+
+                elf = elf.MoveCircular(1);
+            }
+            while (elves.Count > 1);
+
+            return elves.First();
+        }
+
+        public long Compute2()
+        {
+            int numElves = 3012210;
+
+            for (int i = 0; i < numElves; i++)
+            {
+                elves.AddLast(i + 1);
+            }
+
+            var elf = elves.First;
+
+            int count = numElves;
+
+            var across = elf.MoveCircular(count / 2);
+
+            do
+            {
+                var newAcross = across.MoveCircular(((count % 2) == 0) ? 1 : 2);
+
+                elves.Remove(across);
+
+                elf = elf.MoveCircular(1);
+
+                count--;
+                across = newAcross;
+            }
+            while (elves.Count > 1);
+
+            return elves.First();
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -15,7 +15,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            long result = new AdventOfCode._2016.Day18().Compute();
+            long result = new AdventOfCode._2016.Day19().Compute2();
         }
     }
 }
