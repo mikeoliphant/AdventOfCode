af673b42bdee7ce4e9ebd5e901d1045601ed8b8d
Mike Oliphant <oliphant@nostatic.org>
2022-05-31 13:48:37 -0700

    Days 16 & 17

---
+++ AdventOfCode/2015/Day16.cs
@@ -0,0 +1,106 @@
+﻿namespace AdventOfCode._2015
+{
+    internal class Day16 : Day
+    {
+        Dictionary<string, int> identified = new Dictionary<string, int>();
+
+        Dictionary<string, int>[] allSues = new Dictionary<string, int>[500];
+
+        void ReadInput()
+        {
+            foreach (string line in File.ReadLines(@"C:\Code\AdventOfCode\Input\2015\Day16Data.txt"))
+            {
+                string[] split = line.Split(": ");
+
+                identified[split[0]] = int.Parse(split[1]);
+            }
+
+            foreach (string line in File.ReadLines(DataFile))
+            {
+                string[] split = line.Split(": ", 2);
+
+                int sueNumber = int.Parse(split[0].Split(" ")[1]) - 1;
+
+                string[] props = split[1].Split(", ");
+
+                allSues[sueNumber] = new Dictionary<string, int>();
+
+                foreach (string prop in props)
+                {
+                    string[] propSplit = prop.Split(": ");
+
+                    allSues[sueNumber][propSplit[0]] = int.Parse(propSplit[1]);
+                }
+            }
+        }
+
+        public override long Compute()
+        {
+            ReadInput();
+
+            for (int sueNum = 0; sueNum < allSues.Length; sueNum++)
+            {
+                var sue = allSues[sueNum];
+
+                bool match = true;
+
+                foreach (var prop in identified)
+                {
+                    if (sue.ContainsKey(prop.Key) && (sue[prop.Key] != prop.Value))
+                    {
+                        match = false;
+
+                        break;
+                    }
+                }
+
+                if (match)
+                    return sueNum + 1;
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        bool Match(Dictionary<string, int> sue, string key, int value)
+        {
+            if ((key == "cats") || (key == "trees"))
+            {
+                return sue[key] > value;
+            }
+            
+            if ((key == "pomeranians") || (key == "goldfish"))
+            {
+                return sue[key] < value;
+            }
+
+            return sue[key] == value;
+        }
+
+        public override long Compute2()
+        {
+            ReadInput();
+
+            for (int sueNum = 0; sueNum < allSues.Length; sueNum++)
+            {
+                var sue = allSues[sueNum];
+
+                bool match = true;
+
+                foreach (var prop in identified)
+                {
+                    if (sue.ContainsKey(prop.Key) && !Match(sue, prop.Key, prop.Value))
+                    {
+                        match = false;
+
+                        break;
+                    }
+                }
+
+                if (match)
+                    return sueNum + 1;
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ AdventOfCode/2015/Day17.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode._2015
+{
+    internal class Day17 : Day
+    {
+        //int[] containers = new int[] { 20, 15, 10, 5, 5 };
+        int[] containers = null;
+
+        IEnumerable<int[]> NumWaysToFill(int[] fills, int pos, int left)
+        {
+            if (pos == (containers.Length - 1))
+            {
+                if (containers[pos] == left)
+                {
+                    fills[pos] = left;
+
+                    yield return fills;
+                }
+
+                if (left == 0)
+                {
+                    fills[pos] = 0;
+
+                    yield return fills;
+                }
+            }
+            else
+            {
+                fills[pos] = 0;
+
+                foreach (var result in NumWaysToFill(fills, pos + 1, left))
+                {
+                    yield return result;
+                }
+
+                fills[pos] = left;
+
+                foreach (var result in NumWaysToFill(fills, pos + 1, left - containers[pos]))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        public override long Compute()
+        {
+            containers = File.ReadAllLines(DataFile).ToInts().ToArray();
+
+            int toStore = 150;
+
+            return NumWaysToFill(new int[containers.Length], 0, toStore).Count();
+        }
+
+        public override long Compute2()
+        {
+            containers = File.ReadAllLines(DataFile).ToInts().ToArray();
+
+            int toStore = 150;
+
+            var result = NumWaysToFill(new int[containers.Length], 0, toStore);
+
+            int min = result.Min(r => r.Where(c => c != 0).Count());
+
+            return result.Where(r => r.Where(c => c != 0).Count() == min).Count();
+        }
+    }
+}
