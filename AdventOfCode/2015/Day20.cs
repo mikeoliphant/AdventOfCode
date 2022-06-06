779f39a4e68c80b814d3f0f9d4b01d1e2f53bc77
Mike Oliphant <oliphant@nostatic.org>
2022-06-06 11:17:53 -0700

    Day 20

---
+++ AdventOfCode/2015/Day20.cs
@@ -0,0 +1,73 @@
+﻿namespace AdventOfCode._2015
+{
+    internal class Day20 : Day
+    {
+        public override long Compute()
+        {
+            long presents = 29000000;
+
+            int house = 1;
+
+            do
+            {
+                long housePresents = 0;
+
+                foreach (int factor in FactorHelper.AllFactors(house))
+                {
+                    housePresents += (factor * 10);
+                }
+
+                if (housePresents >= presents)
+                    return house;
+
+                house++;
+            }
+            while (true);
+        }
+
+        Dictionary<int, int> numHousesForElf = new Dictionary<int, int>();
+
+        bool IsDelivering(int elf)
+        {
+            if (!numHousesForElf.ContainsKey(elf))
+            {
+                numHousesForElf[elf] = 1;
+
+                return true;
+            }
+
+            if (numHousesForElf[elf] == 50)
+                return false;
+
+            numHousesForElf[elf]++;
+
+            return true;
+        }
+
+        public override long Compute2()
+        {
+            long presents = 29000000;
+
+            int house = 1;
+
+            do
+            {
+                long housePresents = 0;
+
+                foreach (int factor in FactorHelper.AllFactors(house))
+                {
+                    if (IsDelivering(factor))
+                    {
+                        housePresents += (factor * 11);
+                    }
+                }
+
+                if (housePresents >= presents)
+                    return house;
+
+                house++;
+            }
+            while (true);
+        }
+    }
+}
---
+++ AdventOfCode/FactorHelper.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode
+{
+    public static class FactorHelper
+    {
+        public static IEnumerable<(int, int)> PrimeFactors(int n)
+        {
+            int num2 = 0;
+
+            while (n % 2 == 0)
+            {
+                num2++;
+
+                n /= 2;
+            }
+
+            if (num2 > 0)
+                yield return (2, num2);
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                int numI = 0;
+
+                while (n % i == 0)
+                {
+                    numI++;
+
+                    n /= i;
+                }
+
+                if (numI > 0)
+                    yield return (i, numI);
+            }
+
+            if (n > 2)
+                yield return (n, 1);
+        }
+
+        public static IEnumerable<int> AllFactors(int n)
+        {
+            var factors = PrimeFactors(n).ToList();
+
+            if (factors.Count == 0)
+                return new List<int> { n };
+            else
+            {
+                return GetFactorCombos(factors, 0);
+            }
+        }
+
+        static IEnumerable<int> GetFactorCombos(List<(int Factor, int NumTimes)> factors, int pos)
+        {
+            if (pos == (factors.Count - 1))
+            {
+                for (int i = 0; i <= factors[pos].NumTimes; i++)
+                {
+                    yield return (int)Math.Pow(factors[pos].Factor, i);
+                }
+            }
+            else
+            {
+                foreach (var combo in GetFactorCombos(factors, pos + 1))
+                {
+                    for (int i = 0; i <= factors[pos].NumTimes; i++)
+                    {
+                        yield return (int)Math.Pow(factors[pos].Factor, i) * combo;
+                    }
+                }
+            }
+        }       
+    }
+}
