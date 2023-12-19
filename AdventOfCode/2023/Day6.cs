9860f34c04d07319383b77051dc0e7eefeed33ac
Mike Oliphant <oliphant@nostatic.org>
2023-12-19 15:15:18 -0800

    Days 6-7

---
+++ AdventOfCode/2023/Day6.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode._2023
+{
+    internal class Day6 : Day
+    {
+        public override long Compute()
+        {
+            var lines = File.ReadLines(DataFile).ToArray();
+
+            int[] times = Regex.Split(lines[0], @"\s+").Skip(1).ToInts().ToArray();
+            int[] distances = Regex.Split(lines[1], @"\s+").Skip(1).ToInts().ToArray();
+
+            int prod = 1;
+
+            for (int i = 0; i < times.Length; i++)
+            {
+                int numWaysToWin = 0;
+
+                for (int hold = 0; hold <= times[i]; hold++)
+                {
+                    int dist = hold * (times[i] - hold);
+
+                    if (dist > distances[i])
+                    {
+                        numWaysToWin++;
+                    }
+                }
+
+                prod *= numWaysToWin;
+            }
+
+            return prod;
+        }
+
+        public override long Compute2()
+        {
+            var lines = File.ReadLines(DataFile).ToArray();
+
+            long time = long.Parse(lines[0].Split(':')[1].Replace(" ", ""));
+            long distance = long.Parse(lines[1].Split(':')[1].Replace(" ", ""));
+
+            long numWaysToWin = 0;
+
+            for (long hold = 0; hold <= time; hold++)
+            {
+                long dist = hold * (time - hold);
+
+                if (dist > distance)
+                {
+                    numWaysToWin++;
+                }
+            }
+
+            return base.Compute2();
+        }
+    }
+}
---
+++ AdventOfCode/2023/Day7.cs
@@ -0,0 +1,180 @@
+﻿namespace AdventOfCode._2023
+{
+    internal class Day7 : Day
+    {
+        static Dictionary<char, int> cardRank = new();
+
+        int HandScore(string hand)
+        {
+            Dictionary<char, int> ofAKind = new Dictionary<char, int>();
+
+            foreach (char c in hand)
+            {
+                if (!ofAKind.ContainsKey(c))
+                    ofAKind[c] = 1;
+                else
+                    ofAKind[c]++;
+            }
+
+            var sorted = ofAKind.Values.Order().ToArray();
+
+            int handRank = 0;
+
+            if (sorted.Contains(5))
+                handRank = 6;
+            else if (sorted.Contains(4))
+                handRank = 5;
+            else if (sorted.Contains(3))
+                handRank = sorted.Contains(2) ? 4 : 3;
+            else if (sorted.Count(2) == 2)
+                handRank = 2;
+            else if (sorted.Contains(2))
+                handRank = 1;
+
+            return handRank;
+        }
+
+        class CardComparer : IComparer<string>
+        {
+            public int Compare(string a, string b)
+            {
+                for (int pos = 0; pos < a.Length; pos++)
+                {
+                    int diff = cardRank[a[pos]] - cardRank[b[pos]];
+
+                    if (diff != 0)
+                        return diff;
+                }
+
+                return 0;
+            }
+        }
+
+        void Init()
+        {
+            int rank = 0;
+
+            for (char c = '2'; c <= '9'; c++)
+            {
+                cardRank[c] = rank++;
+            }
+
+            cardRank['T'] = rank++;
+            cardRank['J'] = rank++;
+            cardRank['Q'] = rank++;
+            cardRank['K'] = rank++;
+            cardRank['A'] = rank++;
+        }
+
+        public override long Compute()
+        {
+            Init();
+
+            Dictionary<string, int> handScore = new();
+            Dictionary<string, int> handBid = new();
+
+            foreach (string hand in File.ReadLines(DataFile))
+            {
+                string[] split = hand.Split(' ');
+
+                handScore[split[0]] = HandScore(split[0]);
+
+                handBid[split[0]] = int.Parse(split[1]);
+            }
+
+            var sorted = handScore.OrderBy(h => h.Value).ThenBy(h => h.Key, new CardComparer());
+
+            long winnings = 0;
+
+            int rank = 1;
+
+            foreach (var hand in sorted)
+            {
+                winnings += handBid[hand.Key] * rank;
+
+                rank++;
+            }
+
+            return winnings;
+        }
+
+        int HandScoreJokersWild(string hand)
+        {
+            int maxRank = 0;
+
+            foreach (char j in cardRank.Keys)
+            {
+                if (j == 'J')
+                    continue;
+
+                Dictionary<char, int> ofAKind = new Dictionary<char, int>();
+
+                foreach (char c in hand)
+                {
+                    char cj = c;
+
+                    if (cj == 'J')
+                        cj = j;
+
+                    if (!ofAKind.ContainsKey(cj))
+                        ofAKind[cj] = 1;
+                    else
+                        ofAKind[cj]++;
+                }
+
+                var sorted = ofAKind.Values.Order().ToArray();
+
+                int handRank = 0;
+
+                if (sorted.Contains(5))
+                    handRank = 6;
+                else if (sorted.Contains(4))
+                    handRank = 5;
+                else if (sorted.Contains(3))
+                    handRank = sorted.Contains(2) ? 4 : 3;
+                else if (sorted.Count(2) == 2)
+                    handRank = 2;
+                else if (sorted.Contains(2))
+                    handRank = 1;
+
+                maxRank = Math.Max(handRank, maxRank);
+            }
+
+            return maxRank;
+        }
+
+        public override long Compute2()
+        {
+            Init();
+
+            cardRank['J'] = -1;
+
+            Dictionary<string, int> handScore = new();
+            Dictionary<string, int> handBid = new();
+
+            foreach (string hand in File.ReadLines(DataFile))
+            {
+                string[] split = hand.Split(' ');
+
+                handScore[split[0]] = HandScoreJokersWild(split[0]);
+
+                handBid[split[0]] = int.Parse(split[1]);
+            }
+
+            var sorted = handScore.OrderBy(h => h.Value).ThenBy(h => h.Key, new CardComparer());
+
+            long winnings = 0;
+
+            int rank = 1;
+
+            foreach (var hand in sorted)
+            {
+                winnings += handBid[hand.Key] * rank;
+
+                rank++;
+            }
+
+            return winnings;
+        }
+    }
+}
