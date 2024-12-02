5f2f636bdf460b59297c16c8a90f896005b117b0
Mike Oliphant <oliphant@nostatic.org>
2024-12-02 08:14:57 -0800

    2024, Day 2

---
+++ AdventOfCode/2023/Day21.cs
@@ -0,0 +1,90 @@
+﻿namespace AdventOfCode._2023
+{
+    internal class Day21 : Day
+    {
+        Grid<char> grid = null;
+        Grid<char> outGrid = null;
+        Grid<int> minStepGrid = null;
+
+        void ReadData()
+        {
+            grid = new Grid<char>().CreateDataFromRows(File.ReadLines(DataFile));
+            minStepGrid = new Grid<int>(grid.Width, grid.Height);
+            outGrid = new Grid<char>(grid);
+        }
+
+        void DoSteps(int steps, (int X, int Y) startPos)
+        {
+            if ((steps % 2) == 0)
+            {
+                outGrid[startPos] = 'O';
+            }
+            else
+            {
+                outGrid[startPos] = 'x';
+            }
+
+            if (minStepGrid[startPos] >= steps)
+                return;
+
+            minStepGrid[startPos] = steps;
+
+            if (steps == 0)
+                return;
+
+            foreach (var next in grid.ValidNeighbors(startPos.X, startPos.Y))
+            {
+                if ((grid[next] == '.'))
+                {
+                    DoSteps(steps - 1, next);
+                }
+            }
+        }
+
+        public override long Compute()
+        {
+            ReadData();
+
+            //grid.PrintToConsole();
+
+            var startPos = grid.FindValue('S').First();
+
+            grid[startPos] = '.';
+
+            int size = 11;
+
+            Grid<char> newGrid = new(grid.Width * size, grid.Height * size);
+
+            foreach (var cell in grid.GetAll())
+            {
+                char value = grid[cell];
+
+                for (int y = 0; y < size; y++)
+                {
+                    for (int x = 0; x < size; x++)
+                    {
+                        newGrid[(x * grid.Width) + cell.X, (y * grid.Height) + cell.Y] = value;
+                    }
+                }
+            }
+
+            startPos = (((size / 2) * grid.Width) + startPos.X, ((size / 2) * grid.Height) + startPos.Y);
+
+            newGrid[startPos] = 'S';
+
+            grid = newGrid;
+
+            minStepGrid = new Grid<int>(grid.Width, grid.Height);
+
+            outGrid = new Grid<char>(grid);
+
+            DoSteps(262, startPos);
+
+            //outGrid.PrintToConsole();
+
+            long num = outGrid.FindValue('O').Count();
+
+            return num;
+        }
+    }
+}
---
+++ AdventOfCode/2024/Day2.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode._2024
+{
+    internal class Day2 : Day
+    {
+        bool IsSafe(IEnumerable<int> ints)
+        {
+            var deltas = ints.Zip(ints.Skip(1), (current, next) => next - current);
+
+            int sign = 0;
+
+            foreach (int delta in deltas)
+            {
+                if ((Math.Sign(delta) * sign) == -1)
+                    return false;
+
+                if (Math.Abs(delta) > 3)
+                    return false;
+
+                if (delta == 0)
+                    return false;
+
+                sign = Math.Sign(delta);
+            }
+
+            return true;
+        }
+
+        public override long Compute()
+        {
+            long numSafe = 0;
+
+            foreach (string line in File.ReadLines(DataFileTest))
+            {
+                var ints = line.ToInts(' ');
+
+                if (IsSafe(ints))
+                    numSafe++;
+            }
+
+            return numSafe;
+        }
+
+        public override long Compute2()
+        {
+            long numSafe = 0;
+
+            foreach (string line in File.ReadLines(DataFile))
+            {
+                var ints = line.ToInts(' ');
+
+                bool isSafe = false;
+
+                if (IsSafe(ints))
+                {
+                    isSafe = true;
+                }
+                else
+                {
+                    int len = ints.Count();
+
+                    for (int pos = 0; pos < len; pos++)
+                    {
+                        List<int> newInts = ints.ToList();
+
+                        newInts.RemoveAt(pos);
+
+                        if (IsSafe(newInts))
+                        {
+                            isSafe = true;
+
+                            break;
+                        }
+                    }
+                }
+
+                if (isSafe)
+                    numSafe++;
+            }
+
+            return numSafe;
+        }
+    }
+}
