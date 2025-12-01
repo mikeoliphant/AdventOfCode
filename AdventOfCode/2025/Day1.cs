aa1647eb9976139822def53be5b26dd94c742ee9
Mike Oliphant <oliphant@nostatic.org>
2025-12-01 07:49:05 -0800

    Update to .Net 10. 2025, Day 1.

---
+++ AdventOfCode/2025/Day1.cs
@@ -0,0 +1,55 @@
+﻿
+namespace AdventOfCode._2025
+{
+    internal class Day1 : Day
+    {
+        public override long Compute()
+        {
+            int dialPos = 50;
+            int numZeros = 0;
+
+            foreach (string line in File.ReadLines(DataFile))
+            {
+                int dist = int.Parse(line.Substring(1));
+
+                if (line[0] == 'L')
+                {
+                    dist = -dist;
+                }
+
+                dialPos = ModHelper.PosMod(dialPos + dist, 100);
+
+                if (dialPos == 0)
+                    numZeros++;
+            }
+
+            return numZeros;
+        }
+
+        public override long Compute2()
+        {
+            int dialPos = 50;
+            int numZeros = 0;
+
+            foreach (string line in File.ReadLines(DataFile))
+            {
+                int dist = int.Parse(line.Substring(1));
+
+                if (line[0] == 'L')
+                {
+                    dist = -dist;
+                }
+
+                for (int i = 0; i < Math.Abs(dist); i++)
+                {
+                    dialPos = ModHelper.PosMod(dialPos + Math.Sign(dist), 100);
+
+                    if (dialPos == 0)
+                        numZeros++;
+                }
+            }
+
+            return numZeros;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -14,7 +14,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            long result = new AdventOfCode._2024.Day24().Compute2();
+            long result = new AdventOfCode._2025.Day1().Compute2();
         }
     }
 }
